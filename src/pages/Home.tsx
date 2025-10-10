e3b5e03c3b4a9726aeeaae0861736d0e6d49bdb8
Pratik D <tiptoppratik@gmail.com>
2025-10-10 16:36:08 +0100

    feat: create and join game buttons added to home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router";
 const Home = () => {
+  const navigate = useNavigate();
   return (
-    <>
-      <div>Home Component</div>
-    </>
+    <div className="home-page">
+      <Button onClick={() => navigate("/app/game?type=create")}>Create New Game</Button>
+      <Button onClick={() => navigate("/app/game?type=join")}>Join Game</Button>
+    </div>
   );
 };
 
