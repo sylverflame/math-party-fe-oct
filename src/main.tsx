1ba58fec82598a01ece1f1c58bf02eec483fca5d
Pratik D <tiptoppratik@gmail.com>
2025-10-10 16:32:13 +0100

    refactor: react strict mode removed

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,13 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import { BrowserRouter } from "react-router";
 import App from "./App.tsx";
 import { UserContextProvider } from "./contexts/UserContext.tsx";
-import { BrowserRouter } from "react-router";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
     <BrowserRouter>
       <UserContextProvider>
         <App />
       </UserContextProvider>
     </BrowserRouter>
-  </StrictMode>
 );
