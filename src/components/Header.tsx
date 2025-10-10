449332866eb47e26b8859f38a0b88980e86bbdd0
Pratik D <tiptoppratik@gmail.com>
2025-10-10 22:20:06 +0100

    refactor: added home link to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,10 @@
 import { useUser } from "@/contexts/UserContext";
 import { Button } from "./ui/button";
 import { Avatar, AvatarFallback } from "./ui/avatar";
+import { useNavigate } from "react-router";
 
 const Header = () => {
+    const navigate = useNavigate();
   const {
     user: { userId },
     logout,
@@ -11,13 +13,16 @@ const Header = () => {
     logout();
   };
   return (
-    <div className="header-component h-12 w-full bg-gray-800 shadow-md flex items-center justify-end">
-      <Avatar>
-        <AvatarFallback>{userId?.substring(0, 2).toUpperCase()}</AvatarFallback>
-      </Avatar>
-      <Button variant="outline" onClick={onClickLogout} className="mx-2">
-        Logout
-      </Button>
+    <div className="header-component h-12 w-full bg-gray-800 shadow-md flex items-center justify-between px-2">
+        <Button variant="outline" onClick={() => navigate("/app/home")}>Home</Button>
+      <div className="flex items-center gap-2">
+          <Avatar>
+            <AvatarFallback>{userId?.substring(0, 2).toUpperCase()}</AvatarFallback>
+          </Avatar>
+          <Button variant="outline" onClick={onClickLogout}>
+            Logout
+          </Button>
+      </div>
     </div>
   );
 };
