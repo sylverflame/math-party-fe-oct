069d235f4e93b7f2951422f13e997fa22e9a639c
Pratik D <tiptoppratik@gmail.com>
2025-10-10 16:32:37 +0100

    feat: avatar added to header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,21 @@
 import { useUser } from "@/contexts/UserContext";
 import { Button } from "./ui/button";
+import { Avatar, AvatarFallback } from "./ui/avatar";
 
 const Header = () => {
-  const { logout } = useUser();
+  const {
+    user: { userId },
+    logout,
+  } = useUser();
   const onClickLogout = () => {
     logout();
   };
   return (
-    <div className="header-component h-12 w-full bg-gray-800 shadow-md">
-      <Button variant="ghost" onClick={onClickLogout}>
+    <div className="header-component h-12 w-full bg-gray-800 shadow-md flex items-center justify-end">
+      <Avatar>
+        <AvatarFallback>{userId?.substring(0, 2).toUpperCase()}</AvatarFallback>
+      </Avatar>
+      <Button variant="outline" onClick={onClickLogout} className="mx-2">
         Logout
       </Button>
     </div>
