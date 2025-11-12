348c73b4ee4b0e4852d8c8634f9db9fe4a818a7f
Pratik D <tiptoppratik@gmail.com>
2025-11-12 16:14:34 +0000

    refactor: validation added to joinroom room code field

--- src/pages/JoinGame.tsx
+++ src/pages/JoinGame.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import type { ClientMessageType } from "@/hooks/useWebSocket";
 import { useState } from "react";
+import { toast } from "sonner";
 
 type JoinGameProps = {
   sendMessage: (type: ClientMessageType, payload: Record<string, any>) => void;
@@ -11,13 +12,17 @@ const JoinGame = ({ sendMessage }: JoinGameProps) => {
   const [roomCode, setRoomCode] = useState("");
   const onJoinGame = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    if (roomCode.length !== 5) {
+      toast.error("Invalid room code!");
+      return;
+    }
     sendMessage("JOIN_ROOM", {
       roomCode,
     });
   };
   return (
     <form onSubmit={(e) => onJoinGame(e)} className="fade-in">
-      <Input placeholder="Room Code" onChange={(e) => setRoomCode(e.target.value)} />
+      <Input placeholder="Room Code" onChange={(e) => setRoomCode(e.target.value)} maxLength={5}/>
       <Button type="submit" className="mt-4">
         Join Game
       </Button>
