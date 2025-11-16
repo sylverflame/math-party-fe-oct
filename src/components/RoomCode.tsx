3c2f987d9d16a96b36ba37c86ebc820dd880c594
Pratik D <tiptoppratik@gmail.com>
2025-11-16 13:29:38 +0000

    refactor: roomcode format updated based on device width

--- src/components/RoomCode.tsx
+++ src/components/RoomCode.tsx
@@ -1,9 +1,12 @@
+import { useDevice } from "@/contexts/DeviceContext";
 import { useGame } from "@/contexts/GameContext";
+import { ScreenSizes } from "@/types";
 import { useRef } from "react";
 import { toast } from "sonner";
 
 const RoomCode = () => {
   const { gameState } = useGame();
+  const { screenWidth } = useDevice();
   const roomCodeRef = useRef<HTMLDivElement>(null);
 
   const onClick = async () => {
@@ -12,17 +15,24 @@ const RoomCode = () => {
     }
     await navigator.clipboard.writeText(roomCodeRef.current.innerText);
     toast.success("Copied to clipboard!", {
-        duration: 500
-    })
+      duration: 500,
+    });
   };
-  return (
-    <>
-      <h6 className="text-card-foreground font-bold mb-2">{"Room Code"}</h6>
-      <div ref={roomCodeRef} className="text-muted-foreground border-2 rounded-xl border-border text-2xl p-2 cursor-pointer flex justify-center" onClick={onClick}>
-        {gameState.roomCode}
-      </div>
-    </>
-  );
+
+  if (screenWidth < ScreenSizes.MEDIUM) {
+    return <h6 onClick={onClick} className="uppercase text-xs font-extralight text-muted-foreground">Roomcode - {gameState.roomCode}</h6>;
+  }
+
+  if (screenWidth >= ScreenSizes.MEDIUM) {
+    return (
+      <>
+        <h6 className="text-card-foreground font-bold mb-2">{"Room Code"}</h6>
+        <div ref={roomCodeRef} className="text-muted-foreground border-2 rounded-xl border-border text-2xl p-2 cursor-pointer flex justify-center" onClick={onClick}>
+          {gameState.roomCode}
+        </div>
+      </>
+    );
+  }
 };
 
 export default RoomCode;
