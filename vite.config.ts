959d89a270d422bfd2dbcf771d502850584b0ebf
Pratik D <tiptoppratik@gmail.com>
2025-11-16 15:04:08 +0000

    feat: vite mkcert package added

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,19 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import svgr from "vite-plugin-svgr";
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), svgr()],
+  plugins: [react(), tailwindcss(), svgr(), 
+    // mkcert()
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  server: {
+    host: true, // access it from your mobile/other devices on network
   },
 });
