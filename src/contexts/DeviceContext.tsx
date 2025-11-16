3a5bcceaa0018d7b609cd53887a39e43cb837e35
Pratik D <tiptoppratik@gmail.com>
2025-11-16 09:56:56 +0000

    refactor: device resize useEffect logic moved from app to context, logic added to detect touch device

--- src/contexts/DeviceContext.tsx
+++ src/contexts/DeviceContext.tsx
@@ -1,10 +1,10 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 type DeviceType = "Phone" | "Tablet" | "PC";
 
 type DeviceContextType = {
   screenWidth: number;
-  setScreenWidth: React.Dispatch<React.SetStateAction<number>>;
+  isTouchDevice: boolean;
 };
 
 const DeviceContext = createContext<DeviceContextType | null>(null);
@@ -12,7 +12,30 @@ const DeviceContext = createContext<DeviceContextType | null>(null);
 export const DeviceContextProvider = ({ children }: { children: React.ReactNode }) => {
   const [deviceType, setDeviceType] = useState(null);
   const [screenWidth, setScreenWidth] = useState(0);
-  return <DeviceContext.Provider value={{ screenWidth, setScreenWidth }}>{children}</DeviceContext.Provider>;
+  const [isTouchDevice, setIsTouchDevice] = useState(false);
+
+  useEffect(() => {
+    // Detect touch device
+    const touch = "ontouchstart" in window || navigator.maxTouchPoints > 0;
+    setIsTouchDevice(touch);
+
+    // Handle window resize
+    let timeoutId: number;
+    const onWindowResize = () => {
+      if (timeoutId) clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => {
+        setScreenWidth(window.innerWidth);
+      }, 100);
+    };
+    window.addEventListener("resize", onWindowResize);
+    onWindowResize();
+    return () => {
+      window.removeEventListener("resize", onWindowResize);
+      if (timeoutId) clearTimeout(timeoutId);
+    };
+  }, []);
+
+  return <DeviceContext.Provider value={{ screenWidth, isTouchDevice }}>{children}</DeviceContext.Provider>;
 };
 
 export const useDevice = () => {
