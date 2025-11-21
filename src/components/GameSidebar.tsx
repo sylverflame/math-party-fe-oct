81fc0b8cbfc9062002fb27ff9fb85a6d4df5ceaa
Pratik D <tiptoppratik@gmail.com>
2025-11-21 00:26:51 +0000

    feat: ability to restart game by host added

--- src/components/GameSidebar.tsx
+++ src/components/GameSidebar.tsx
@@ -8,6 +8,10 @@ import NewChatIndicator from "./NewChatIndicator";
 import PlayersList from "./PlayersList";
 import RoomCode from "./RoomCode";
 import SidebarCloseButton from "./SidebarCloseButton";
+import { Button } from "./ui/button";
+import { VscDebugRestart } from "react-icons/vsc";
+import { useUser } from "@/contexts/UserContext";
+import { useGame } from "@/contexts/GameContext";
 
 interface IGameSidebar {
   sendMessage: (type: ClientMessageType, payload: Record<string, any>) => void;
@@ -36,7 +40,7 @@ const GameSidebar = ({ sendMessage, onClose, showSidebar }: IGameSidebar) => {
   return (
     <div className={cn(sidebarClasses.commonClasses, sidebarClasses.smallScreen, showSidebar ? "block" : "hidden", sidebarClasses.mediumScreen)}>
       <RoomCode />
-      <SidebarCloseButton onClose={onClose} className="absolute right-4 top-4 md:hidden"/>
+      <SidebarCloseButton onClose={onClose} className="absolute right-4 top-4 md:hidden" />
       <Tabs defaultValue="players" className="mt-4" onValueChange={onTabChange}>
         <TabsList>
           <TabsTrigger value="players">{"Players"}</TabsTrigger>
@@ -52,8 +56,35 @@ const GameSidebar = ({ sendMessage, onClose, showSidebar }: IGameSidebar) => {
           <Chatroom sendMessage={sendMessage} />
         </TabsContent>
       </Tabs>
+      <RestartButton sendMessage={sendMessage} />
     </div>
   );
+};
+
+interface IRestartButton {
+  sendMessage: (type: ClientMessageType, payload: Record<string, any>) => void;
+}
+
+const RestartButton = ({ sendMessage }: IRestartButton) => {
+  const {
+    user: { userId },
+  } = useUser();
+  const {
+    gameState: { host, roomCode, status },
+  } = useGame();
+
+  const onRestart = () => {
+    sendMessage("RESTART_GAME", {
+      roomCode,
+    });
+  };
+  if (userId === host && status === "GAME_IN_PROGRESS") {
+    return (
+      <Button className="mt-2 mx-auto" onClick={onRestart}>
+        <VscDebugRestart /> Restart
+      </Button>
+    );
+  }
 };
 
 export default GameSidebar;
