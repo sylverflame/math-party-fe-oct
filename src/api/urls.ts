09039e6a4191118e5d6e1897e08a6ffe7816bdfc
Pratik D <tiptoppratik@gmail.com>
2025-11-21 00:27:34 +0000

    refactor: google auth api url updated

--- src/api/urls.ts
+++ src/api/urls.ts
@@ -1,6 +1,6 @@
 
 const BE_SERVER = import.meta.env.VITE_BE_SERVER;
 export const API_URLS = {
-    GOOGLE_AUTH: `${BE_SERVER}/api/v1/auth/google`,
+    GOOGLE_AUTH: `${BE_SERVER}/api/v1/google`,
     LOGIN: `${BE_SERVER}/api/v1/auth/login`,
 }
